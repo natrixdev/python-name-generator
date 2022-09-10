0bf0f9b842c0eb054ad4fde9b3e4b363298dec3f
natrix_dev <discordlenko@gmail.com>
2022-09-10 10:37:39 +0200

    Create reference.py

---
+++ reference.py
@@ -0,0 +1,8 @@
+# This file will only tell to github this repo supports python ! 
+
+import time 
+while True:
+  print('github/natrixdev')
+  time.sleep(0.2)
+  print('Star this repo')
+  print('https://github.com/natrixdev/python-name-generator/')
